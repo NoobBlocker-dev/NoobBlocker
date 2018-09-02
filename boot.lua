0cffc7cc6f3a6a49794d03440d5fdb7c92d75edf
= <=>
2018-09-01 18:40:19 -0700

    release push?

--- boot.lua
+++ boot.lua
@@ -1,6 +1,5 @@
 NoobBlocker = LibStub("AceAddon-3.0"):NewAddon("NoobBlocker", "AceComm-3.0", "AceHook-3.0", "AceEvent-3.0")
 
-
 function NoobBlocker:OnInitialize()
     self:SetupConfig()
     self:SetupHooks()
